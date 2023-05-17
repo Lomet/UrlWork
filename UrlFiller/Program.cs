3c2b540cc97afcef58ee467ca197b383e9f7794b
Stan Goldin <salty87@gmail.com>
2023-05-17 23:38:47 +0300

    more text

--- UrlFiller/Program.cs
+++ UrlFiller/Program.cs
@@ -4,10 +4,9 @@
 // for example : "https://api.covalenthq.com/v1/[ChainId]/events/address/[ContractAddress]/?starting-block=[StartingBlock]&ending-block=[EndingBlock]&page-number=[PageNumber]&page-size=[MaxPageNumber]&key=[Key]"
 // will be made into : "https://api.covalenthq.com/v1/1/events/address/0x7Fc66500c84A76Ad7e9c93437bFc5Ac33E2DDaE9/?starting-block=0&ending-block=99999999&page-number=0&page-size=99999999&key=ckey_1234567890"
 // using flurl.http url builder
+// see UnitTest1.cs for an example of how to use this
 internal class Program
 {
-    
-
     static void Main(string[] args)
     {
         Console.WriteLine("Hello, World!");
