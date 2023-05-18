30d99073aab5298e8bb13fcc97f34b4eb0756225
Stan Goldin <salty87@gmail.com>
2023-05-18 14:22:08 +0300

    add NeedTrailingSlash option

--- UrlFiller/URLParser.cs
+++ UrlFiller/URLParser.cs
@@ -6,27 +6,36 @@ namespace UrlFiller
     public class URLParser
     {
         private readonly Dictionary<string, IValueResolver> valueResolvers;
+        private readonly char[] chars = new char[] { '[', ']' };
+        private bool NeedTrailingSlash { get; set; }
 
-        public URLParser(Dictionary<string, IValueResolver> valueResolvers)
+        public URLParser(Dictionary<string, IValueResolver> valueResolvers, bool trailing = false)
         {
             this.valueResolvers = valueResolvers;
+            NeedTrailingSlash = trailing;
         }
 
         public Url GetOutputUrl(string Url) => GetOutputUrl(new Url(Url));
 
-        public Url GetOutputUrl(Url url) => new Url(url.Root)
-                .AppendPathSegments(url.PathSegments.Select(GetRealValue))
-                .SetQueryParams(url.QueryParams.Select(p =>
-                    new KeyValuePair<string, string>(p.Name, GetRealValue(p.Value.ToString()))));
+        public Url GetOutputUrl(Url url)
+        {
+            var result = new Url(url.Root)
+               .AppendPathSegments(url.PathSegments.Select(GetRealValue))
+               .SetQueryParams(url.QueryParams.Select(p =>
+                   new KeyValuePair<string, string>(p.Name, GetRealValue(p.Value.ToString()))));
+            if (NeedTrailingSlash)
+                result = result.AppendPathSegment("/");
+            return result;
+        }
 
         private string GetRealValue(string? paramName)
         {
             if (paramName is null)
                 return string.Empty;
 
-            if (paramName.StartsWith('[') && paramName.EndsWith(']'))
+            if (paramName.StartsWith(chars[0]) && paramName.EndsWith(chars[1]))
             {
-                paramName = paramName.Trim('[', ']');
+                paramName = paramName.Trim(chars[0], chars[1]);
                 if (valueResolvers.TryGetValue(paramName, out var resolver))
                     return resolver.GetValue(paramName);
 
